ab0d1a290f32de54250300de585c9139e0dd806f
huziafa <huzaifaiqbal5554@gmail.com>
2023-07-21 03:18:35 +0500

    complete get sqaure of a number program

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 // start tow numbers  add program static or dynamicly
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++>
 // static add num program 
-let nmuOne = 2;
-let numTow = 5;
-function addNumStaticly(){
-    return nmuOne + numTow;
-};
-let resultAddNumStatic = addNumStaticly();
-console.log(resultAddNumStatic);
+// let nmuOne = 2;
+// let numTow = 5;
+// function addNumStaticly(){
+//     return nmuOne + numTow;
+// };
+// let resultAddNumStatic = addNumStaticly();
+// console.log(resultAddNumStatic);
 
-// dynamic add num program 
+// // dynamic add num program 
 
-let numberOne = +prompt("put first a number which you want to add");
-let numberTow = +prompt("put second a number which you want to add");
-function addNumdynamicly(){
-    return numberOne + numberTow;
-};
-let resultAddNumDynamicly = addNumdynamicly();
-console.log(resultAddNumDynamicly)
+// let numberOne = +prompt("put first a number which you want to add");
+// let numberTow = +prompt("put second a number which you want to add");
+// function addNumdynamicly(){
+//     return numberOne + numberTow;
+// };
+// let resultAddNumDynamicly = addNumdynamicly();
+// console.log(resultAddNumDynamicly)
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++>
 // end tow numbers  add program static or dynamicly
 
 
 
-
+// start get squre root of a number 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++>
+let a = +prompt("enter a number to get sqaure root of your puted number")
+let b = Math.sqrt(a)
+let c = Math.round(b)
+console.log(c)
+// start get squre root of a number 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++>
 
 
