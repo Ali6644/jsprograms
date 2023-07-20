ca75078405e1283274153b20df5ef47095bc6f6d
huziafa <huzaifaiqbal5554@gmail.com>
2023-07-21 03:08:04 +0500

    complete add a number staticly and dynamicly program

---
+++ main.js
@@ -0,0 +1,28 @@
+// start tow numbers  add program static or dynamicly
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++>
+// static add num program 
+let nmuOne = 2;
+let numTow = 5;
+function addNumStaticly(){
+    return nmuOne + numTow;
+};
+let resultAddNumStatic = addNumStaticly();
+console.log(resultAddNumStatic);
+
+// dynamic add num program 
+
+let numberOne = +prompt("put first a number which you want to add");
+let numberTow = +prompt("put second a number which you want to add");
+function addNumdynamicly(){
+    return numberOne + numberTow;
+};
+let resultAddNumDynamicly = addNumdynamicly();
+console.log(resultAddNumDynamicly)
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++>
+// end tow numbers  add program static or dynamicly
+
+
+
+
+
+
